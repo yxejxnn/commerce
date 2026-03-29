6d18a68d5acc165d514f050110c7be9e331c6a7e
정예진 <yxejxnn@gmail.com>
2026-03-29 17:13:28 +0900

    Lv 1 Cart 장바구니에 담긴 상품의 총 금액 계산 메서드 추가

--- src/Cart.java
+++ src/Cart.java
@@ -15,4 +15,18 @@ public class Cart {
     public List<CartItem> getCartItemList() {
         return cartItemList;
     }
+    // 장바구니에 담긴 상품들의 총 금액을 계산하는 메서드
+    public int calculateTotalPrice() {
+        int cartTotalPrice = 0; // 합계를 담을 변수
+
+        for (CartItem cartItem : cartItemList) {
+            // cartItem - 카트에 담긴, .getCartProduct() - 상품에서, .getProductPrice() - 상품의 가격을 가져옴
+            int productPrice = cartItem.getCartProduct().getProductPrice();
+            // cartItem - 카트에 담긴, .getCartQuantity() - 상품의 수량을 가져옴
+            int productQuantity = cartItem.getCartQuantity();
+            // 장바구니에 담기는 상품의 가격과 수량을 곱해서 누적 시킴
+            cartTotalPrice += productPrice * productQuantity;
+        }
+        return cartTotalPrice; // 계산된 총 금액 반환
+    }
 }
