1277b7f1d700f21cf5538ec108b9f81ef84332bd
정예진 <yxejxnn@gmail.com>
2026-03-29 12:51:09 +0900

    Spet.3 CommerceSystem - start 카테고리 목록 출력 및 번호 입력 시 해당 카테고리 상품 목록 출력

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class CommerceSystem {
 
     // 속성
-    List<Product> productList; // 상품 목록
+    List<Category> categoryList; // 카테고리 목록
 
     // 입력받기 위한 Scanner 객체 생성 (달릴 때만 입이 생기는 고양이는 되면 안 됨...)
     Scanner scanner = new Scanner(System.in);
 
     // 생성자
-    public CommerceSystem(List<Product> productList) {
-        this.productList = productList;
+    public CommerceSystem(List<Category> categoryList) {
+        this.categoryList = categoryList;
     }
 
     // 기능
@@ -21,37 +21,50 @@ public class CommerceSystem {
         while (true) {
 
             // 프로그램 실행 시 상품 목록 출력
-            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+            System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
 
-            // 향상된 for문으로 List 안에 상품(객체)을 하나씩 꺼내서 출력
+            // 향상된 for문으로 List 안에 카테고리 목록을 하나씩 꺼내서 출력
             // 향상된 for문은 인덱스 번호가 없기 때문에 번호를 저장할 변수를 따로 만듦
-            int number = 1;
+            int categoryNumber = 1;
 
-            for (Product product : productList) {
-                System.out.println(number + ". " + product.productName + " | " + String.format("%,d" , product.productPrice) + "원 | " + product.productDescription);
-                number++;
+            // 카테고리 목록 출력
+            for (Category category : categoryList) {
+                System.out.println(categoryNumber + ". " + category.getCategoryName());
+                categoryNumber++;
             }
 
             System.out.println("0. 종료 | 프로그램 종료"); // 종료 메뉴 출력
             System.out.println(); // 줄바꿈
             System.out.print("메뉴 입력: ");
-            int productChoice = scanner.nextInt(); // 상품 메뉴 선택
+            int categoryChoice = scanner.nextInt(); // 카테고리 메뉴 선택
             System.out.println(); // 줄바꿈
 
             // 0 입력 시 종료
-            if (productChoice == 0) {
+            if (categoryChoice == 0) {
                 System.out.println("커머스 플랫폼을 종료합니다.");
                 break;
             }
-            else if (productChoice >= 1 && productChoice <= productList.size()) {
-                // 입력한 번호에 맞는 상품을 가져옴
-                Product selectedProduct = productList.get(productChoice - 1);
-                // 선택한 상품 정보 출력
-                System.out.println("선택한 상품: " + selectedProduct.productName + " | " + String.format("%,d" , selectedProduct.productPrice) + "원 | " + selectedProduct.productDescription);
-                System.out.println();
+            // 카테고리 번호를 입력한 경우
+            if (categoryChoice >= 1 && categoryChoice <= categoryList.size()) {
+                // 입력한 카테고리 객체를 가져옴
+                Category selectedCategory = categoryList.get(categoryChoice - 1); // 리스트 인덱스는 0부터 시작하기 때문에 1을 빼줌
+                // 선택한 카테고리 출력
+                System.out.println("[ " + selectedCategory.categoryName + " 카테고리 ]");
+
+                // 향산된 for문으로 List 안에 상품 목록을 하나씩 꺼내서 출력
+                // 향산된 for문은 인덱스 번호가 없기 때문에 번호를 저장할 변수를 따로 만듦
+                int productNumber = 1;
+
+                // 선택한 카테고리에 들어있는 상품 목록 출력
+                for (Product product : selectedCategory.productList) {
+                    // String.foramt("%,c")를 사용해서 금액 출력 시 천단위 구분
+                    System.out.println(productNumber + ". " + product.productName + " | " + String.format("%,d" , product.productPrice) + "원 | " + product.productDescription);
+                    productNumber++;
+                }
+                System.out.println(); // 줄바꿈
             }
             else {
-                System.out.println("잘못된 입력입니다.");
+                System.out.println("잘못된 카테고리 번호입니다.");
                 System.out.println(); // 줄바꿈
             }
         }
