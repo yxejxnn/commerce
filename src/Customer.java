6800333b96d6f65128c799431b10e31ab9f3c372
정예진 <yxejxnn@gmail.com>
2026-03-29 11:47:51 +0900

    Spet.3 Customer 클래스 속성 생성자 작성

---
+++ src/Customer.java
@@ -0,0 +1,14 @@
+public class Customer {
+
+    // 속성
+    String customerName; // 고객명
+    String customerEmail; // 고객 이메일
+    String customerGrade; // 고객 등급
+
+    // 생성자
+    public Customer(String customerName, String customerEmail, String customerGrade) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerGrade = customerGrade;
+    }
+}
