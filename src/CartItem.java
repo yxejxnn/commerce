00f194eb21f6488116135688e3b91db94d562486
정예진 <yxejxnn@gmail.com>
2026-03-29 17:05:20 +0900

    Lv 1 CartItem 클래스 속성 수정

--- src/CartItem.java
+++ src/CartItem.java
@@ -1,20 +1,21 @@
 public class CartItem {
 
     // 속성
-    private String cartProduct; // 장바구니에 담긴 상품
+    private Product cartProduct; // 장바구니에 담긴 상품
     private int cartQuantity; // 장바구니에 담긴 상품 수량
 
     // 생성자
-    public CartItem(String cartProduct, int cartQuantity) {
+    public CartItem(Product cartProduct, int cartQuantity) {
         this.cartProduct = cartProduct;
         this.cartQuantity = cartQuantity;
     }
 
     // 기능
     // 장바구니에 담긴 상품 반환
-    public String getCartProduct() {
+    public Product getCartProduct() {
         return cartProduct;
     }
+
     // 장바구니에 담긴 상품 수량 반환
     public int getCartQuantity() {
         return cartQuantity;
